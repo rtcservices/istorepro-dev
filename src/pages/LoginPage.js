80cd68b8ddd70b2ed62aa3f83f4a10dc9aaf9caf
Naveen <naveen.chakiath@live.com>
2021-06-21 21:02:49 +0530

    Clean up code

--- src/pages/LoginPage.js
+++
@@ -1,7 +0,0 @@
-import Login from "../components/Login/Login";
-
-const LoginPage = (props) => {
-  return <Login onLogin={props.onLogin} onLocaleChange={props.onLocaleChange}/>;
-};
-
-export default LoginPage;
