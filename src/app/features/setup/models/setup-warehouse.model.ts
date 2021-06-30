242fae8ab7c55905a145a9e95c7a858c81026895
Krishna Raj K. P <krishnaraj.kp@rtcservices.in>
2021-06-30 23:57:44 +0530

    Ware house pages

--- src/app/features/setup/models/setup-warehouse.model.ts
+++ src/app/features/setup/models/setup-warehouse.model.ts
@@ -3,3 +3,25 @@ export interface SetupWarehouseModel{
     companyName: string;
   }
   
+  export interface SetupWarehouseDocumentModel{
+    type: string;
+    prefix: string;
+    currentNo: number;
+  }
+
+  export interface SetupWarehouseRuleBoxModel{
+    serialNo: number;
+    rule: string;
+    value: string;
+  }
+
+  export interface SetupWarehouseContactModel{
+    name: string;
+    email: string;
+  }
+
+  export interface SetupWarehouseOperatorModel{
+    code: string;
+    name: string;
+    loginName: string;
+  }
--- src/app/features/setup/setup-warehouse/setup-warehouse.component.ts
+++ src/app/features/setup/setup-warehouse/setup-warehouse.component.ts
@@ -5,7 +5,7 @@ import { MatTabGroup } from '@angular/material/tabs';
 
 import { TranslateService } from '@ngx-translate/core';
 import { LoaderService } from '../../../services/loader.service';
-import { SetupWarehouseModel } from '../models/setup-warehouse.model';
+import { SetupWarehouseModel, SetupWarehouseDocumentModel, SetupWarehouseContactModel, SetupWarehouseRuleBoxModel, SetupWarehouseOperatorModel } from '../models/setup-warehouse.model';
 import { TitleService } from '../../../services/title.service';
 import { SiteTranslateService } from '../../../services/site-translate.service';
 
@@ -22,11 +22,74 @@ export class SetupWarehouseComponent implements OnInit {
 
   dataWarehouseForm!: FormGroup;
   warehouseDataModel!: SetupWarehouseModel;
+  documentDataModel!: SetupWarehouseDocumentModel;
+  contactDataModel!: SetupWarehouseContactModel;
+  ruleBoxDataModel!: SetupWarehouseRuleBoxModel;
+  operatorDataModel!: SetupWarehouseOperatorModel;
 
   formErrorTranslated = '';
   filterErrorTranslated = '';
 
-  displayedColumns = ['type', 'prefix', 'currentNo', 'autoEmail'];
+  displayedDocumentColumns = ['type', 'prefix', 'currentNo', 'autoEmail'];
+  displayedRuleBoxColumns = ['serialNo', 'rule', 'value'];
+  displayedContactColumns = ['name', 'email'];
+  displayedOperatorColumns = ['code', 'name', 'loginName', 'inactive'];
+
+  documentDataSource: SetupWarehouseDocumentModel[] = [];
+  dummyDocumentModel: SetupWarehouseDocumentModel[] = [
+    { type: 'Type 1', prefix: 'pr1', currentNo : 1001 },
+    { type: 'Type 2', prefix: 'pr2', currentNo : 1002 },
+    { type: 'Type 3', prefix: 'pr3', currentNo : 1003 },
+    { type: 'Type 4', prefix: 'pr4', currentNo : 1004 },
+    { type: 'Type 5', prefix: 'pr5', currentNo : 1005 },
+    { type: 'Type 6', prefix: 'pr6', currentNo : 1006 },
+    { type: 'Type 7', prefix: 'pr7', currentNo : 1007 },
+    { type: 'Type 8', prefix: 'pr8', currentNo : 1008 },
+    { type: 'Type 9', prefix: 'pr9', currentNo : 1009 },
+    { type: 'Type 10', prefix: 'pr10', currentNo : 1010 },
+  ];
+
+  ruleBoxDataSource: SetupWarehouseRuleBoxModel[] = [];
+  dummyRuleBoxModel: SetupWarehouseRuleBoxModel[] = [
+    {serialNo: 1, rule: 'Rule 1', value: 'Value 1' },
+    {serialNo: 2, rule: 'Rule 2', value: 'Value 2' },
+    {serialNo: 3, rule: 'Rule 3', value: 'Value 3' },
+    {serialNo: 4, rule: 'Rule 4', value: 'Value 4' },
+    {serialNo: 5, rule: 'Rule 5', value: 'Value 5' },
+    {serialNo: 6, rule: 'Rule 6', value: 'Value 6' },
+    {serialNo: 7, rule: 'Rule 7', value: 'Value 7' },
+    {serialNo: 8, rule: 'Rule 8', value: 'Value 8' },
+    {serialNo: 9, rule: 'Rule 9', value: 'Value 9' },
+    {serialNo: 10, rule: 'Rule 10', value: 'Value 10' },
+  ];
+
+  contactDataSource: SetupWarehouseContactModel[] = [];
+  dummyContactModel: SetupWarehouseContactModel[] = [
+    { name: 'Contact 1', email: 'contact1@hmail.com' },
+    { name: 'Contact 2', email: 'contact2@hmail.com' },
+    { name: 'Contact 3', email: 'contact3@hmail.com' },
+    { name: 'Contact 4', email: 'contact4@hmail.com' },
+    { name: 'Contact 5', email: 'contact5@hmail.com' },
+    { name: 'Contact 6', email: 'contact6@hmail.com' },
+    { name: 'Contact 7', email: 'contact7@hmail.com' },
+    { name: 'Contact 8', email: 'contact8@hmail.com' },
+    { name: 'Contact 9', email: 'contact9@hmail.com' },
+    { name: 'Contact 10', email: 'contact10@hmail.com' },
+  ];
+
+  operatorDataSource: SetupWarehouseOperatorModel[] = [];
+  dummyoperatorModel: SetupWarehouseOperatorModel[] = [
+    { code: 'Code 1', name: 'Operator 1', loginName : 'login 1' },
+    { code: 'Code 2', name: 'Operator 2', loginName : 'login 2' },
+    { code: 'Code 3', name: 'Operator 3', loginName : 'login 3' },
+    { code: 'Code 4', name: 'Operator 4', loginName : 'login 4' },
+    { code: 'Code 5', name: 'Operator 5', loginName : 'login 5' },
+    { code: 'Code 6', name: 'Operator 6', loginName : 'login 6' },
+    { code: 'Code 7', name: 'Operator 7', loginName : 'login 7' },
+    { code: 'Code 8', name: 'Operator 8', loginName : 'login 8' },
+    { code: 'Code 9', name: 'Operator 9', loginName : 'login 9' },
+    { code: 'Code 10', name: 'Operator 10', loginName : 'login 10' },
+  ];
 
   constructor(
     private fb: FormBuilder,
@@ -45,6 +108,11 @@ export class SetupWarehouseComponent implements OnInit {
       this.formErrorTranslated = this.translate.instant('error.form');
       this.filterErrorTranslated = this.translate.instant('error.filter');
     });
+
+    this.documentDataSource  = [...this.dummyDocumentModel];
+    this.ruleBoxDataSource  = [...this.dummyRuleBoxModel];
+    this.contactDataSource  = [...this.dummyContactModel];
+    this.operatorDataSource  = [...this.dummyoperatorModel];
   }
 
   CreateDataWarehouseForm() {
@@ -131,5 +199,12 @@ export class SetupWarehouseComponent implements OnInit {
 
   get dataWarehouseControls() {
     return this.dataWarehouseForm.controls;
+  }
+
+  saveCompanyData() {}
+  deleteCompanyData() {}
+
+  resetCompanyDataForm() {
+    this.dataWarehouseForm.reset();
   }
 }
