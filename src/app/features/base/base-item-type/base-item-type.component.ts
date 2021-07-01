e8f2bf8969bcfb96ab8560de43dbbb1b5a6eeaff
Krishna Raj K. P <krishnaraj.kp@rtcservices.in>
2021-07-01 10:48:51 +0530

    Itemtype setup

--- src/app/features/base/base-item-type/base-item-type.component.ts
+++ src/app/features/base/base-item-type/base-item-type.component.ts
@@ -1,4 +1,16 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
+
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { MatTabGroup } from '@angular/material/tabs';
+
+import { TranslateService } from '@ngx-translate/core';
+import { LoaderService } from '../../../services/loader.service';
+
+import { TitleService } from '../../../services/title.service';
+import { SiteTranslateService } from '../../../services/site-translate.service';
+
+import { patternsHelper } from '../../../helpers/patterns.helper';
+import { NotificationService } from 'src/app/services/notification.service';
 
 @Component({
   selector: 'ibe-base-item-type',
@@ -6,10 +18,57 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./base-item-type.component.scss']
 })
 export class BaseItemTypeComponent implements OnInit {
+  @ViewChild('baseItemTypeTab', { static: false })
+  setupCompanyTab!: MatTabGroup;
 
-  constructor() { }
+  dataItemTypeForm!: FormGroup
+
+  formErrorTranslated = '';
+  filterErrorTranslated = '';
+
+  constructor(
+    private fb: FormBuilder,
+    private loader: LoaderService,
+    private siteTranslateService: SiteTranslateService,
+    private translate: TranslateService,
+    private titleService: TitleService,
+    private notification: NotificationService
+  ) {
+    this.createDataWarehouseForm();
+   }
 
   ngOnInit(): void {
+    this.titleService.changeTitleTranslated('menu.setupCompany');
+    const language = this.siteTranslateService.defaultLanguage;
+    this.translate.use(language).subscribe((res) => {
+      this.formErrorTranslated = this.translate.instant('error.form');
+      this.filterErrorTranslated = this.translate.instant('error.filter');
+    });
   }
 
+  createDataWarehouseForm() {
+    this.dataItemTypeForm = this.fb.group({
+      code: '',
+      description: '',
+      storageType: '',
+      alertDays: '',
+      mandatory: '',
+      expiry: '',
+      manufacturingDate: '',
+      batchNo: '',
+      consolidateOn: '',
+      defaultPick: '',
+    });
+  }
+
+  get dataItemTypeControls() {
+    return this.dataItemTypeForm.controls;
+  }
+
+  saveCompanyData() {}
+  deleteCompanyData() {}
+
+  resetCompanyDataForm() {
+    this.dataItemTypeForm.reset();
+  }
 }
