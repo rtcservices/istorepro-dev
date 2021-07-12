927dfce6b3abf6130b7eb51aee52fe3f19635003
Krishna Raj K. P <krishnaraj.kp@rtcservices.in>
2021-07-12 21:36:46 +0530

    Receipt page setup

---
+++ src/app/features/query/models/query-transaction.model.ts
@@ -0,0 +1,20 @@
+export interface QueryTransactionReceiptModel {
+    wh: string;
+    owner: string;
+    grn: string;
+    date: string;
+    poRefNo: string;
+    origin: string;
+    status: string;
+  }
+
+  export interface QueryTransactionDeliveryModel {
+    wh: string;
+    owner: string;
+    drn: string;
+    Date: string;
+    shipTo: string;
+    do: string;
+    so: string;
+    dt: string;
+  }
--- src/app/features/query/query-transactions/query-transactions.component.ts
+++ src/app/features/query/query-transactions/query-transactions.component.ts
@@ -1,4 +1,12 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { TranslateService } from '@ngx-translate/core';
+import { patternsHelper } from '../../../helpers/patterns.helper';
+import { LoaderService } from '../../../services/loader.service';
+import { NotificationService } from '../../../services/notification.service';
+import { SiteTranslateService } from '../../../services/site-translate.service';
+import { TitleService } from '../../../services/title.service';
+import { QueryTransactionDeliveryModel, QueryTransactionReceiptModel } from '../models/query-transaction.model';
 
 @Component({
   selector: 'ibe-query-transactions',
@@ -7,9 +15,129 @@ import { Component, OnInit } from '@angular/core';
 })
 export class QueryTransactionsComponent implements OnInit {
 
-  constructor() { }
+  formErrorTranslated = '';
+  filterErrorTranslated = '';
 
-  ngOnInit(): void {
+  receiptForm!: FormGroup;
+  deliveryForm!: FormGroup;
+
+  receiptDataSource: QueryTransactionReceiptModel[] = [];
+  dummyReceiptModel: QueryTransactionReceiptModel[] = [
+    { wh : "Warehouse" , owner : "Owner", grn : "GRN", date : "Date", poRefNo : "Po/Ref No", status: "status", origin: "Origin"},
+    { wh : "Warehouse" , owner : "Owner", grn : "GRN", date : "Date", poRefNo : "Po/Ref No", status: "status", origin: "Origin"},
+  ];
+
+  deliveryDataSource: QueryTransactionDeliveryModel[] = [];
+  dummyDeliveryModel: QueryTransactionDeliveryModel[] = [
+    { wh : "Warehouse" , owner : "Owner", shipTo : "Ship To", Date : "Date", do : "DO", so: "SO", dt : "DT", drn : "DRN"},
+    { wh : "Warehouse" , owner : "Owner", shipTo : "Ship To", Date : "Date", do : "DO", so: "SO", dt : "DT", drn : "DRN"}
+  ];
+
+  constructor(
+    private fb: FormBuilder,
+    private loader: LoaderService,
+    private siteTranslateService: SiteTranslateService,
+    private translate: TranslateService,
+    private titleService: TitleService,
+    private notification: NotificationService
+  ) { 
+    this.createDeliveryForm();
+    this.createReceiptForm();
   }
 
+  ngOnInit(): void {
+    this.titleService.changeTitleTranslated('menu.setupWarehouse');
+    const language = this.siteTranslateService.defaultLanguage;
+    this.translate.use(language).subscribe((res) => {
+      this.formErrorTranslated = this.translate.instant('error.form');
+      this.filterErrorTranslated = this.translate.instant('error.filter');
+    });
+
+    this.receiptDataSource = this.dummyReceiptModel;
+    this.deliveryDataSource = this.dummyDeliveryModel;
+
+  }
+
+  createReceiptForm() {
+    this.receiptForm = this.fb.group({
+      warehouse: [
+        '',
+        [Validators.required, Validators.pattern(patternsHelper.alphanumeric)]
+      ],
+      owner: [
+        '',
+        [Validators.maxLength(10),Validators.pattern(patternsHelper.alphanumeric)]
+      ],
+      item: [
+        '',
+        [Validators.maxLength(50), Validators.pattern(patternsHelper.alphanumeric)]
+      ],
+      batchNo: [
+        '',
+        [Validators.maxLength(35), Validators.pattern(patternsHelper.alphanumeric)]
+      ],
+      poRefNo: [
+        '',
+        [Validators.maxLength(18), Validators.pattern(patternsHelper.alphanumeric)]
+      ],
+      origin: '', 
+      status: '',
+      grn: [
+        '',
+        [Validators.maxLength(18), Validators.pattern(patternsHelper.alphanumeric)]
+      ],
+      receiptFrom: '', 
+      receiptTo: '', 
+      stock: ''
+    });
+  }
+
+  get receiptFormControls() {
+    return this.receiptForm.controls;
+  }
+
+  onReceiptSubmit() {}
+
+  openOwnerDialog() {
+  }
+
+  createDeliveryForm() {
+    this.deliveryForm = this.fb.group({
+      warehouse: [
+        '',
+        [Validators.required, Validators.pattern(patternsHelper.alphanumeric)]
+      ],
+      owner: [
+        '',
+        [Validators.maxLength(10),Validators.pattern(patternsHelper.alphanumeric)]
+      ],
+      item: [
+        '',
+        [Validators.maxLength(50), Validators.pattern(patternsHelper.alphanumeric)]
+      ],
+      batchNo: [
+        '',
+        [Validators.maxLength(35), Validators.pattern(patternsHelper.alphanumeric)]
+      ],
+      poRefNo: [
+        '',
+        [Validators.maxLength(18), Validators.pattern(patternsHelper.alphanumeric)]
+      ],
+      origin: '', 
+      status: '',
+      grn: [
+        '',
+        [Validators.maxLength(18), Validators.pattern(patternsHelper.alphanumeric)]
+      ],
+      receiptFrom: '', 
+      receiptTo: '', 
+      stock: ''
+    });
+  }
+
+  get deliveryFormControls() {
+    return this.deliveryForm.controls;
+  }
+
+  onDeliverySubmit() {}
 }
