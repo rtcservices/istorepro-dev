09f86ffc81d416f0360114bb38b0862db435ce2d
Naveen <naveen.chakiath@live.com>
2021-07-12 19:08:17 +0530

    Add settings component

--- src/app/features/settings/settings.module.ts
+++ src/app/features/settings/settings.module.ts
@@ -3,15 +3,10 @@ import { CommonModule } from '@angular/common';
 
 import { SettingsRoutingModule } from './settings-routing.module';
 import { SettingsProfileComponent } from './settings-profile/settings-profile.component';
-
+import { SettingsComponent } from './settings.component';
 
 @NgModule({
-  declarations: [
-    SettingsProfileComponent
-  ],
-  imports: [
-    CommonModule,
-    SettingsRoutingModule
-  ]
+  declarations: [SettingsProfileComponent, SettingsComponent],
+  imports: [CommonModule, SettingsRoutingModule]
 })
-export class SettingsModule { }
+export class SettingsModule {}
