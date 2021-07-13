3ca6f6a79e6beb6a3264fa39d188105ebb82cc1a
Krishna Raj K. P <krishnaraj.kp@rtcservices.in>
2021-07-13 18:00:42 +0530

    Build files

--- src/environments/environment.test.ts
+++ src/environments/environment.test.ts
@@ -10,8 +10,8 @@ export const environment = {
   production: false,
   test: true,
   appName: 'iStoreProBE',
-  appVersion: 'v1.0.0.39',
-  appTimeStamp: '13-07-21 12:05:14 PM',
+  appVersion: 'v1.0.0.40',
+  appTimeStamp: '13-07-21 5:41:39 PM',
   i18nPrefix: '',
   versions: {
     app: packageJson.version,
