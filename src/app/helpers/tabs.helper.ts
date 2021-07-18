cc391c7598bd553ea31b10a0d0c1551655f3a3f8
Naveen <naveen.chakiath@live.com>
2021-07-18 19:13:54 +0530

    finished till setup module

--- src/app/helpers/tabs.helper.ts
+++ src/app/helpers/tabs.helper.ts
@@ -2,7 +2,7 @@ export function setTabHeights(dataTabBody: string) {
   const tabCardBody = document.querySelectorAll(dataTabBody);
   if (!tabCardBody) return;
   const maxHeight =
-    2 +
+    6 +
     Math.max(...Array.from(tabCardBody).map((elm: any) => elm.clientHeight));
   tabCardBody.forEach((itm) => {
     itm.setAttribute('style', `height:${maxHeight}px;`);
